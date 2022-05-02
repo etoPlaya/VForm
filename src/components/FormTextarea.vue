8a33a505b403c3e6f217e6602ebba2d28ed0cb6b
EtoPlaya <tm@goldcarrot.ru>
2022-05-02 21:22:49 +0500

    feat(VTextarea): VTextarea release v1

---
+++ src/components/FormTextarea.vue
@@ -0,0 +1,49 @@
+<template>
+  <form 
+    novalidate
+    class="v-form" 
+  >
+    <VTextarea 
+      :value="form[textarea.id]"
+      :placeholder="textarea.placeholder"
+      :label="textarea.label"
+      @change-value="changeFieldValue($event, textarea.id)"
+    />
+  </form>
+</template>
+
+<script>
+import VTextarea from '@/components/UI/VTextarea';
+
+export default { 
+  name: 'FormTextarea',
+  components: {
+    VTextarea,
+  },
+
+  data: () => ({
+    form: {
+      message: '',
+    },
+  }),
+  computed: {
+    textarea() {
+      return {
+        id: 'message',
+        placeholder: 'Сообщение',
+        label: 'Ввидите сообщение',
+      }
+    },
+  },
+
+  methods: {
+    changeFieldValue(value, fieldId) {
+      this.form[fieldId] = value;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
