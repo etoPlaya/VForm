6ed232672f46422448d084d5ee4556986dc428f3
EtoPlaya <tm@goldcarrot.ru>
2022-05-17 01:41:58 +0500

    feat(VCheckbox): VCheckbox release v1

---
+++ components/FormCheckbox.vue
@@ -0,0 +1,47 @@
+<template>
+  <form 
+    novalidate
+    class="v-form" 
+  >
+    <VCheckbox 
+      :value="form[checkbox.id]"
+      :label="checkbox.label"
+      @change-value="changeFieldValue($event, checkbox.id)"
+    />
+  </form>
+</template>
+
+<script>
+import VCheckbox from '@/components/UI/VCheckbox';
+
+export default { 
+  name: 'FormCheckbox',
+  components: {
+    VCheckbox,
+  },
+
+  data: () => ({
+    form: {
+      checkboxChecked: false,
+    },
+  }),
+  computed: {
+    checkbox() {
+      return {
+        id: 'checkboxChecked',
+        label: 'Согласитесь написано странно',
+      }
+    },
+  },
+
+  methods: {
+    changeFieldValue(value, fieldId) {
+      this.form[fieldId] = value;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
